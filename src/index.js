cc1a5a233706e8bc2e3e60e82445c8aea901bf03
Robin Ljungqvist <Robin9006@hotmail.com>
2018-09-08 01:42:27 +0200

    React Fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
 if (module.hot) {
   module.hot.accept();
 }
 
-const MainApp = () => {
-  <h1>Hello React!</h1>
+const MainApp = () => <h1>Hello React!</h1>;
 
-  render(<MainApp />, document.getElementById('app'))
-}
+render(<MainApp />, document.getElementById('app'));
